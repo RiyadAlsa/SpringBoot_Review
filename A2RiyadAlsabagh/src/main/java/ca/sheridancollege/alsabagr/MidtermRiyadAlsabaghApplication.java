e55395a12e248e1fcd6bb506b30d0ea504f7d4f4
Riyad Alsabagh <59846101+RiyadAlsa@users.noreply.github.com>
2021-03-03 17:14:49 -0500

    Add files via upload

---
+++ A2RiyadAlsabagh/src/main/java/ca/sheridancollege/alsabagr/MidtermRiyadAlsabaghApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.alsabagr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermRiyadAlsabaghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermRiyadAlsabaghApplication.class, args);
+	}
+
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Run the project with spring boot
+Tyep into the URL localhost:8090
+Enter the name, date, review and add it
